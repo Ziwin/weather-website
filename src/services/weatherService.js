ed064df10688b9a84477f14a8d9529f6e02cd283
Zi Win <zlim2@vols.utk.edu>
2022-11-27 01:40:28 -0500

    Updated Weather Website

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,13 +1,16 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "ENTER YOUR API KEY";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 // https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
 
 const getWeatherData = (infoType, searchParams) => {
+  console.log(process.env)
   const url = new URL(BASE_URL + "/" + infoType);
   url.search = new URLSearchParams({ ...searchParams, appid: API_KEY });
+
 
   return fetch(url).then((res) => res.json());
 };
